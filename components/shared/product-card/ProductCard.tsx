f9e9ff10291d744de296c5b2837a370c1058d16f
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2024-08-25 16:16:49 +0600

    product updated

--- components/shared/product-card/ProductCard.tsx
+++ components/shared/product-card/ProductCard.tsx
@@ -1,10 +1,21 @@
 import { IProduct } from '@/app/_types';
 import Image from 'next/image';
 
-export const ProductCard = ({ image }: IProduct) => {
+export const ProductCard = ({
+  image,
+  title,
+  email,
+  payable_price,
+  price,
+  stock,
+}: IProduct) => {
   return (
     <div className='bg-white p-6'>
       <Image src={image} width={300} height={300} alt='Image' />
+      <div className='mt-6 flex items-center justify-between'>
+        <h1 className='text-base font-semibold'>{title}</h1>
+        <p className='text-lg font-bold'>${price}</p>
+      </div>
     </div>
   );
 };
